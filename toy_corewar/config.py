c874544e2ee44bdeffb50014b7b8226213bf2813
Amric TRUDEL <atrudel@student.42.fr>
2018-09-18 16:08:51 +0200

    Fix multiprocessing issues

--- toy_corewar/config.py
+++ toy_corewar/config.py
@@ -1,25 +1,17 @@
 import torch
 
-# Toy Corewar settings
-NUM_ACTIONS = 225
-NUM_REGISTERS = 4
-MAX_LENGTH = 5
-N_INSTRUCTIONS = 4
-N_VARS = 3
-N_VALS = 20
-N_TARGETS = 4
+# Hardware usage
+CUDA = torch.cuda.is_available() # boolean
+MULTIPROCESSING = True
 
 # Neural network
-CUDA = torch.cuda.is_available() # boolean
 LR = 0.01
-
 
 # DQN parameters
 REPLAY_BUFFER_SIZE = 100000
 LEARNING_STARTS = 100
 LEARNING_FREQ = 4
 BATCH_SIZE = 32
-
 GAMMA = 0.99
 # Epsilon is scheduled
 TARGET_UPDATE_FREQ = 1000
@@ -28,3 +20,12 @@ TARGET_UPDATE_FREQ = 1000
 # Logging parameter
 LOG_FREQ = 100
 SAVE_FREQ = 5000 #Not implemented yet
+
+# Toy Corewar settings
+NUM_ACTIONS = 225
+NUM_REGISTERS = 4
+MAX_LENGTH = 5
+N_INSTRUCTIONS = 4
+N_VARS = 3
+N_VALS = 20
+N_TARGETS = 4
--- toy_corewar/main.py
+++ toy_corewar/main.py
@@ -10,7 +10,7 @@ def run_experiment(reward_func, episodes, root_dir):
     log_dir = os.path.join(root_dir, reward_func.__name__)
     os.makedirs(log_dir)
     Q = Dueling_DQN()
-    train(Q, reward_func, episodes, log_dir=log_dir)
+    train(Q, reward_func, episodes, log_dir=log_dir, verbose=True)
     final_save_path = os.path.join(log_dir, "models", "final")
     torch.save(Q.state_dict(), final_save_path)
     
@@ -21,7 +21,15 @@ def run_experiment_series(name, reward_functions, episodes):
     
     if isinstance(episodes, int):
         episodes = [episodes] * len(reward_functions)  
-    with Pool(processes=len(reward_functions)) as pool:
-        pool.starmap(run_experiment, zip(reward_functions, episodes, repeat(root_dir)))
-        
-run_experiment_series("03-VAL_scalar_encoding", reward_functions, 50000)
+    
+    if MULTIPROCESSING:
+        multiprocessing.set_start_method('spawn')
+        with Pool(processes=len(reward_functions)) as pool:
+            pool.starmap(run_experiment, zip(reward_functions, episodes, repeat(root_dir)))
+    else:
+        for reward_func, ep in zip(reward_functions, episodes):
+            run_experiment(reward_func, ep, root_dir)
+
+            
+if __name__ == '__main__':
+    run_experiment_series("test", reward_functions, 10000)
